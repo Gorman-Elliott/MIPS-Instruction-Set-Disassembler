c998262474eba5f89a7fb7cc45e23b9ceafb80c3
Elliott Gorman <75457087+Gorman-Elliott@users.noreply.github.com>
2021-01-26 21:25:39 -0600

    Updating MISD
    Currently adding coproc functions

--- misd.asm
+++ misd.asm
@@ -45,20 +45,48 @@
 							     .data
 
 # LOOKUP TABLES
-registers        : .asciiz "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"
-registers_off    : .half   0, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62, 66, 70, 74, 78, 82, 86, 90, 94, 98, 102, 106, 110, 114, 118, 122, 126
-opcode           : .asciiz "0", "1", "j", "jal", "beq", "bne", "blez", "bgtz", "addi", "addiu", "slti", "sltiu", "andi", "ori", "xori", "lui", "z=0", "z=1", "z=2", "19", "beql", "bnel", "blezl", "bgtzl", "24", "25", "26", "27", "28", "29", "30", "31", "lb", "lh", "lwl", "lw", "lbu", "lhu", "lwr", "39", "sb", "sh", "swl", "sw", "44", "45", "swr", "cache", "ll", "lwc1", "lwc2", "pref", "52", "ldc1", "ldc2", "55", "sc", "swc1", "swc2", "59", "60", "sdc1", "sdc2", "63"
-opcode_off       : .half   0, 2, 4, 6, 10, 14, 18, 23, 28, 33, 39, 44, 50, 55, 59, 64, 68, 72, 76, 80, 83, 88, 93, 99, 105, 108, 111, 114, 117, 120, 123, 126, 129, 132, 135, 139, 142, 146, 150, 154, 157, 160, 163, 167, 170, 173, 176, 180, 186, 189, 194, 199, 204, 207, 212, 217, 220, 223, 228, 233, 236, 239, 244, 249
-opcode_0         : .asciiz "sll", "1", "srl", "sra", "sllv", "5", "srlv", "srav", "jr", "jalr", "movz", "movn", "syscall", "break", "14", "sync", "mfhi", "mthi", "mflo", "mtlo", "20", "21", "22", "23", "mult", "multu", "div", "divu", "28", "29", "30", "31", "add", "addu", "sub", "subu", "and", "or", "xor", "nor", "40", "41", "slt", "sltu", "44", "45", "46", "47", "tge", "tgeu", "tlt", "tltu", "teq", "53", "tne"
-opcode_0_off     : .half   0, 4, 6, 10, 14, 19, 21, 26, 31, 34, 39, 44, 49, 57, 63, 66, 71, 76, 81, 86, 91, 94, 97, 100, 103, 108, 114, 118, 123, 126, 129, 132, 135, 139, 144, 148, 153, 157, 160, 164, 168, 171, 174, 178, 183, 186, 189, 192, 195, 199, 204, 208, 213, 217, 220
-opcode_0_f_1     : .asciiz "movf", "movt"
-opcode_0_f_1_off : .half   0, 5
-opcode_1         : .asciiz "bltz", "bgez", "bltzl", "bgezl", "4", "5", "6", "7", "tgei", "tgeiu", "tlti", "tltiu", "tegi", "13", "tnei", "15", "bltzal", "bgezal", "bltzall", "bgczall"
-opcode_1_off     : .half   0, 5, 10, 16, 22, 24, 26, 28, 30, 35, 41, 46, 52, 57, 60, 65, 68, 75, 82, 90
-opcode_28        : .asciiz "madd", "maddu", "mul", "3", "msub", "msubu"
-opcode_28_off    : .half   0, 5, 11, 15, 17, 22
-opcode_28_cl     : .asciiz "clz", "clo"
-opcode_28_cl_off : .half   0, 4
+registers               : .asciiz "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"
+registers_off           : .half   0, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62, 66, 70, 74, 78, 82, 86, 90, 94, 98, 102, 106, 110, 114, 118, 122, 126
+registers_cop1		: .asciiz "$f0", "$f1", "$f2", "$f3", "$f4", "$f5", "$f6", "$f7", "$f8", "$f9", "$f10", "$f11", "$f12", "$f13", "$f14", "$f15", "$f16", "$f17", "$f18", "$f19", "$f20", "$f21", "$f22", "$f23", "$f24", "$f25", "$f26", "$f27", "$f28", "$f29", "$f30", "$f31"
+registers_cop1_off	: .half   0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145
+
+opcode                  : .asciiz "0", "1", "j", "jal", "beq", "bne", "blez", "bgtz", "addi", "addiu", "slti", "sltiu", "andi", "ori", "xori", "lui", "z=0", "z=1", "z=2", "19", "beql", "bnel", "blezl", "bgtzl", "24", "25", "26", "27", "28", "29", "30", "31", "lb", "lh", "lwl", "lw", "lbu", "lhu", "lwr", "39", "sb", "sh", "swl", "sw", "44", "45", "swr", "cache", "ll", "lwc1", "lwc2", "pref", "52", "ldc1", "ldc2", "55", "sc", "swc1", "swc2", "59", "60", "sdc1", "sdc2", "63"
+opcode_off              : .half   0, 2, 4, 6, 10, 14, 18, 23, 28, 33, 39, 44, 50, 55, 59, 64, 68, 72, 76, 80, 83, 88, 93, 99, 105, 108, 111, 114, 117, 120, 123, 126, 129, 132, 135, 139, 142, 146, 150, 154, 157, 160, 163, 167, 170, 173, 176, 180, 186, 189, 194, 199, 204, 207, 212, 217, 220, 223, 228, 233, 236, 239, 244, 249
+opcode_0                : .asciiz "sll", "1", "srl", "sra", "sllv", "5", "srlv", "srav", "jr", "jalr", "movz", "movn", "syscall", "break", "14", "sync", "mfhi", "mthi", "mflo", "mtlo", "20", "21", "22", "23", "mult", "multu", "div", "divu", "28", "29", "30", "31", "add", "addu", "sub", "subu", "and", "or", "xor", "nor", "40", "41", "slt", "sltu", "44", "45", "46", "47", "tge", "tgeu", "tlt", "tltu", "teq", "53", "tne"
+opcode_0_off            : .half   0, 4, 6, 10, 14, 19, 21, 26, 31, 34, 39, 44, 49, 57, 63, 66, 71, 76, 81, 86, 91, 94, 97, 100, 103, 108, 114, 118, 123, 126, 129, 132, 135, 139, 144, 148, 153, 157, 160, 164, 168, 171, 174, 178, 183, 186, 189, 192, 195, 199, 204, 208, 213, 217, 220
+opcode_0_f_1            : .asciiz "movf", "movt"
+opcode_0_f_1_off        : .half   0, 5
+opcode_1                : .asciiz "bltz", "bgez", "bltzl", "bgezl", "4", "5", "6", "7", "tgei", "tgeiu", "tlti", "tltiu", "tegi", "13", "tnei", "15", "bltzal", "bgezal", "bltzall", "bgczall"
+opcode_1_off            : .half   0, 5, 10, 16, 22, 24, 26, 28, 30, 35, 41, 46, 52, 57, 60, 65, 68, 75, 82, 90
+
+opcode_16               : .asciiz "mfc0", "1", "cfc0", "3", "mtc0", "5", "ctc0", "7", "8"
+opcode_16_off           : .half   0, 5, 7, 12, 14, 19, 21, 26, 28
+opcode_16_cop0          : .asciiz "0", "tlbr", "tlbwi", "3", "4", "5", "tlbwr", "7", "8", "tlbp"
+opcode_16_cop0_off      : .half   0, 2, 7, 13, 15, 17, 19, 25, 27, 29
+
+opcode_17               : .asciiz "mfc1", "1", "cfc1", "3", "mtc1", "5", "ctc1", "7", "8"
+opcode_17_off           : .half   0, 5, 7, 12, 14, 19, 21, 26, 28
+opcode_17_8             : .asciiz "bc1f", "bc1t", "bc1fl", "bc1tl"
+opcode_17_8_off         : .half   0, 5, 10, 16
+
+opcode_17_cop1_s        : .asciiz "add.s", "sub.s", "mul.s", "div.s", "sqrt.s", "abs.s", "mov.s", "neg.s", "8", "9", "10", "11", "round.w.s", "trunc.w.s", "cell.w.s", "floor.w.s", "16", "17", "movz.s", "movn.s", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "cvt.s.s", "cvt.d.s", "34", "35", "cvt.w.s", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "c.f.s", "c.un.s", "c.eq.s", "c.ueq.s", "c.olt.s", "c.ult.s", "c.ole.s", "c.ule.s", "c.sf.s", "c.ngle.s", "c.seq.s", "c.ngl.s", "c.lt.s", "c.nge.s", "c.le.s", "c.ngt.s"
+opcode_17_cop1_s_off    : .half   0, 6, 12, 18, 24, 31, 37, 43, 49, 51, 53, 56, 59, 69, 79, 88, 98, 101, 104, 111, 118, 121, 124, 127, 130, 133, 136, 139, 142, 145, 148, 151, 154, 162, 170, 173, 176, 184, 187, 190, 193, 196, 199, 202, 205, 208, 211, 214, 217, 223, 230, 237, 245, 253, 261, 269, 277, 284, 293, 301, 309, 316, 324, 331
+opcode_17_cop1_s_17     : .asciiz "movf.s", "movt.s"
+opcode_17_cop1_s_17_off : .half   0, 7
+opcode_17_cop1_d        : .asciiz "add.d", "sub.d", "mul.d", "div.d", "sqrt.d", "abs.d", "mov.d", "neg.d", "8", "9", "10", "11", "round.w.d", "trunc.w.d", "cell.w.d", "floor.w.d", "16", "17", "movz.d", "movn.d", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "cvt.s.d", "cvt.d.d", "34", "35", "cvt.w.d", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "c.f.d", "c.un.d", "c.eq.d", "c.ueq.d", "c.olt.d", "c.ult.d", "c.ole.d", "c.ule.d", "c.sf.d", "c.ngle.d", "c.seq.d", "c.ngl.d", "c.lt.d", "c.nge.d", "c.le.d", "c.ngt.d"
+opcode_17_cop1_d_off    : .half   0, 6, 12, 18, 24, 31, 37, 43, 49, 51, 53, 56, 59, 69, 79, 88, 98, 101, 104, 111, 118, 121, 124, 127, 130, 133, 136, 139, 142, 145, 148, 151, 154, 162, 170, 173, 176, 184, 187, 190, 193, 196, 199, 202, 205, 208, 211, 214, 217, 223, 230, 237, 245, 253, 261, 269, 277, 284, 293, 301, 309, 316, 324, 331
+opcode_17_cop1_d_17     : .asciiz "movf.d", "movt.d"
+opcode_17_cop1_d_17_off : .half   0, 7
+
+opcode_18               : .asciiz "mfc2", "1", "cfc2", "3", "mtc2", "5", "ctc2", "7", "8"
+opcode_18_off           : .half   0, 5, 7, 12, 14, 19, 21, 26, 28
+opcode_18_8             : .asciiz "bc2f", "bc2t", "bc2fl", "bc2tl"
+opcode_18_8_off         : .half   0, 5, 10, 16
+
+opcode_28               : .asciiz "madd", "maddu", "mul", "3", "msub", "msubu"
+opcode_28_off           : .half   0, 5, 11, 15, 17, 22
+opcode_28_cl            : .asciiz "clz", "clo"
+opcode_28_cl_off        : .half   0, 4
 
 # STRING LITERALS
 integers    : .asciiz "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
@@ -509,6 +537,9 @@ decodeInstr:
 	getVar(op)
 	beq $v0, 0,  decode_funct	# opcode == 0, go to decode funct field
 	beq $v0, 1,  decode_target	# opcode == 1, go to decode rt field
+	#beq $v0, 16, decode_zed0	# opcode == 16, z = 0, go to decode rs field
+	#beq $v0, 17, decode_zed1	# opcode == 17, z = 1, go to decode rs field
+	beq $v0, 18, decode_zed2	# opcode == 17, z = 2, go to decode rs field
 	beq $v0, 28, decode_funct_sec	# opcode == 28, go to decode 2ndary funct field
 	
 	# else we have root instructions, immediates
@@ -724,7 +755,103 @@ decodeInstr:
 			# exit
 				j di_exit
 		# END df_sec_mul
-
+	# END decode_funct_sec
+	
+	decode_zed1:
+		getVar(rs)
+		beq $v0, 16, decode_zed1_s	# if rs == 16, singleword precision coproc functs (5:0)
+		beq $v0, 17, decode_zed1_d	# if rs == 17, doubleword precision coproc functs (5:0)
+		
+		
+		decode_zedl_s:
+			
+			getVar(funct_upper)
+			beq $v0, 0, dc_zed1_s_fu0	# if functUpper == 0, basic arithmetic coproc functs
+			beq $v0, 1, dc_zed1_s_fu1	# if functUpper == 1, rounding coproc functs
+			beq $v0, 2, dc_zed1_s_fu2	# if functUpper == 2, mov coproc functs
+			beq $v0, 4, dc_zedl_s_fu4	# if functUpper == 4, cvt coproc functs
+			
+			# else c.cond.fmt functs (funct == 6 && 7)
+			
+			
+			dc_zed1_s_fu2:
+				getVar(funct)
+				# TODOOO
+				# TODOOO
+				# TODOOO WORKING HERE
+			
+			dc_zed1_s_fu4:
+				# print instruction
+					getVar(funct)
+					getStringFromOffset(opcode_17_cop1_s, opcode_17_cop1_s_off, $v0)
+					move $s7, $v0
+					insertspace($s7, $s7)
+					
+				# Get and print fd
+					getBitsInRange(10, 6)
+					getStringFromOffset(registers_cop1, registers_cop1_off, $v0)
+					COPYSTRING($v0, $s7)
+					move $s7, $v0
+					insertCommaAndSpace($s7, $s7)
+					
+				# Get and print fs
+					getBitsInRange(15, 11)
+					getStringFrommOffset(registers_cop1, registers_cop1_off, $v0)
+					COPYSTRING($v0, $s7)
+				
+				# exit
+					j di_exit
+			# END dc_zed1_s_fu4
+		
+	
+	decode_zed2:
+		getVar(rs)
+		beq $v0, 8, decode_zed2_rs8 # if rs == 8, go to bczX instructions
+		
+		# else print instruction
+			getVar(rs)
+			getStringFromOffset(opcode_18, opcode_18_off, $v0)
+			move $s7, $v0
+			insertspace($s7, $s7)
+		
+		# print reg rt
+			getVar(rt)
+			getStringFromOffset(registers, registers_off, $v0)
+			COPYSTRING($v0, $s7)
+			move $s7, $v0
+			insertCommaAndSpace($s7, $s7)
+		
+		# print Impl field (immediate field)
+			getVar(imm)
+			INT_TO_STRING($v0, $s7, 1, 15)
+		
+		# exit
+			j di_exit
+		
+		decode_zed2_rs8:
+			
+			# Print coproc 2 branches
+				getBitsInRange($s0, 17, 16)
+				getStringFromOffset(opcode_18_8, opcode_18_8_off, $v0)
+				COPYSTRING($v0, $s7)
+				move $s7, $v0
+				insertspace($s7, $s7)
+			
+			# print cc (in bits 20-18)
+				getBitsInRange($s0, 20, 18)
+				INT_TO_STRING($v0, $s7, 0, 0)	# Note that the fourth argument here is irrelevant
+				move $s7, $v0
+				insertCommaAndSpace($s7, $s7)
+				
+			# print offset field (immediate field)
+				getVar(imm)
+				INT_TO_STRING($v0, $s7, 1, 15)
+			
+			# exit
+				j di_exit
+		# END decode_zed2_rs8
+	# END decode_zed2
+	
 	decode_target:
 		# print instruction at rt
 			getVar(rt)
@@ -750,11 +877,11 @@ decodeInstr:
 	
 	decode_funct:				# case if op = 0
 		getVar(funct_upper)
-		beq $v0, 0, decode_func_shift	# if funct_upper = 0, shift instructions
-		beq $v0, 1, decode_func_misc	# if funct_upper = 1, decode instructions 8 - 15
-		beq $v0, 2, decode_func_moves	# if funct_upper = 2, decode instructions 16 - 23
-		beq $v0, 3, decode_func_mds	# if funct_upper = 3, decode instructions 24 - 31
-		beq $v0, 6, decode_func_traps	# if funct_upper = 6, decode instructions 48 - 55 
+		beq $v0, 0, decode_func_shift	# if funct_upper == 0, shift instructions
+		beq $v0, 1, decode_func_misc	# if funct_upper == 1, decode instructions 8 - 15
+		beq $v0, 2, decode_func_moves	# if funct_upper == 2, decode instructions 16 - 23
+		beq $v0, 3, decode_func_mds	# if funct_upper == 3, decode instructions 24 - 31
+		beq $v0, 6, decode_func_traps	# if funct_upper == 6, decode instructions 48 - 55 
 		# else instructions must be 32 - 39	
 			# print "%funct "
 				getVar(funct)
@@ -1092,7 +1219,7 @@ readNext32:
 	# Windows .txt format stores a new line using \n\r
 	li $v0, 14
 	la $a1, dumpster
-	li $a2, 2		# for different newline formats, adjust this
+	li $a2, 2		# for different newline formats, adjust this number. Currently dumps the next two characters
 	syscall			# dump \n\r
 	
 	move $v0, $a1		# return address of 32 byte string
@@ -1232,16 +1359,17 @@ str_to_dec:
 # NOTES:
 #	$a2 = 1 if $a0 might be signed, 0 if not
 int_to_str:
-	addi $sp, $sp, -20
-	sw $ra, 16($sp)
-	sw $s0, 12($sp)
-	sw $s1, 8($sp)
-	sw $s2, 4($sp)
-	sw $s3, 0($sp)
+	addi $sp, $sp, -24
+	sw $ra, 20($sp)
+	sw $s0, 16($sp)
+	sw $s1, 12($sp)
+	sw $s2, 8($sp)
+	sw $s3, 4($sp)
+	sw $s4, 0($sp)
 	
-	move $s3, $a1	# save a1
-	li $s0, 10	# s0 = 10; for division ops
-	
+	li $s0, 10		# s0 = 10; for division ops
+	move $s3, $a1		# save a1
+	move $s4, $zero		# count = 0
 	bne $a2, 1, its_next	# If flag != 1, no need to sign extend
 	
 	getBitAt($a0, $a3)	# This is the sign bit
@@ -1264,28 +1392,32 @@ int_to_str:
 		div $a0, $s0		# int / 10
 		mfhi $s1		# s1 = rem
 		addi $s1, $s1, 48	# rem + 48 = ASCII value of digit
-		
 		sb $s1, ($a1)		# store ASCII digit
-		mflo $s2		# s2 = quotient
 		
+		mflo $s2		# s2 = quotient
+		addi $s4, $s4, 1	# count += 1
 		beq $s2, 0, itr_exit	# exit if quotient is 0
-		# else quotient != 0
 		addi $a1, $a1, 1	# increment a1 offset
+		# else quotient != 0
 		move $a0, $s2		# a0 = quotient
 		j itr_loop
 		
 	itr_exit:
 	
-		# reverse string so it is correct order
+		# reverse string so it is in correct order
 		move $a0, $s3		# address to a0
 		jal reverseString	# reverse string
 	
-		lw $s3, 0($sp)
-		lw $s2, 4($sp)
-		lw $s1, 8($sp)
-		lw $s0, 12($sp)
-		lw $ra, 16($sp)
-		addi $sp, $sp, 20
+		# return address (count + original in $s3)
+		add $v0, $s3, $s4	# return = count + addr
+		
+		lw $s4, 0($sp)
+		lw $s3, 4($sp)
+		lw $s2, 8($sp)
+		lw $s1, 12($sp)
+		lw $s0, 16($sp)
+		lw $ra, 20($sp)
+		addi $sp, $sp, 24
 		jr $ra
 # END int_to_str
 
